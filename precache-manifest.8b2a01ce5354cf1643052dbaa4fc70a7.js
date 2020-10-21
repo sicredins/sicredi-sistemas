a5eb86e3a55ba30549836976edfa44b7f350db5b
MARCOS YURI <sicredins@gmail.com>
2020-10-21 11:44:02 -0300

    Updates

---
+++ precache-manifest.8b2a01ce5354cf1643052dbaa4fc70a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac55e47be83d8fd44d093d0248ccc6f1",
+    "url": "/sicredi-sistemas/index.html"
+  },
+  {
+    "revision": "9d5ef2ec24b4d90fd186",
+    "url": "/sicredi-sistemas/static/css/main.fafa8f4d.chunk.css"
+  },
+  {
+    "revision": "418ac35dbe7cae446563",
+    "url": "/sicredi-sistemas/static/js/2.bea2e9d7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/sicredi-sistemas/static/js/2.bea2e9d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d5ef2ec24b4d90fd186",
+    "url": "/sicredi-sistemas/static/js/main.ad71799f.chunk.js"
+  },
+  {
+    "revision": "686255b8cd5d4dc91815",
+    "url": "/sicredi-sistemas/static/js/runtime-main.5c7f1283.js"
+  },
+  {
+    "revision": "1be55c064380b8203a1925230d3adc0e",
+    "url": "/sicredi-sistemas/static/media/logo.1be55c06.svg"
+  }
+]);
