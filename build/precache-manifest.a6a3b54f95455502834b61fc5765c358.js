8aaabc24d787427ceee89fd9c93991d578a3ef55
MARCOS YURI <sicredins@gmail.com>
2020-10-21 11:37:01 -0300

    build fix

---
+++ build/precache-manifest.a6a3b54f95455502834b61fc5765c358.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d342efd8326a38ae110f2d8baee415b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "baac0f854155b3f9c4d0",
+    "url": "/static/css/main.fafa8f4d.chunk.css"
+  },
+  {
+    "revision": "418ac35dbe7cae446563",
+    "url": "/static/js/2.bea2e9d7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.bea2e9d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "baac0f854155b3f9c4d0",
+    "url": "/static/js/main.9571d11a.chunk.js"
+  },
+  {
+    "revision": "867ae4c5de52b42334e9",
+    "url": "/static/js/runtime-main.3c22ff83.js"
+  },
+  {
+    "revision": "1be55c064380b8203a1925230d3adc0e",
+    "url": "/static/media/logo.1be55c06.svg"
+  }
+]);
