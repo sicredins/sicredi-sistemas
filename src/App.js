f28ae845d578f07a34db78f0d33b5871ace83434
MARCOS YURI <sicredins@gmail.com>
2020-10-28 08:12:15 -0300

    lelele

--- src/App.js
+++ src/App.js
@@ -1,162 +1,181 @@
 import React, { useState, useEffect } from 'react';
 import logo from './assets/logo.svg';
 import { FaChrome, FaSafari, FaSearch } from 'react-icons/fa';
-import { FiCircle } from 'react-icons/fi'
 import './App.css';
 
-const links = [
+let links = [
   {
     name: "REDE COLABORATIVA",
-    url: "https://confederacaosicredi.sharepoint.com/sites/rc_731",
+    url: "googlechromes://confederacaosicredi.sharepoint.com/sites/rc_731",
     coop: true,
     cola: false,
     chrome: true,
     safari: true,
-    vpn: false
+    vpn: false,
+    logo: null
   },
   {
-    name: "SISTEMA DE CARTÕES - SGC",
-    url: "http://sicredicartoes.sicredi.net/",
+    name: "SICREDI CARTÕES - SGC",
+    url: "googlechrome://sicredicartoes.sicredi.net/",
     coop: true,
     cola: false,
     safari: false,
     chrome: true,
-    vpn: false
+    vpn: false,
+    logo: null,
   },
   {
-    name: "TOPAZ",
-    url: "http://pld.sicredi.net/",
+    name: "PLD",
+    url: "googlechrome://pld.sicredi.net/",
     coop: false,
     cola: false,
     safari: false,
     chrome: false,
-    vpn: true
+    vpn: true,
+    logo: null,
+    showModal: false
   },
   {
     name: "QUIOSQUE COLABORADOR",
-    url: "https://quiosque.sicredi.com.br",
+    url: "googlechromes://quiosque.sicredi.com.br",
     coop: true,
     cola: true,
     safari: true,
     chrome: true,
-    vpn: false
+    vpn: false,
+    logo: null
   },
   {
     name: "REGISTRO E ACERTO DE PONTO",
-    url: "https://gestaoponto.sicredi.com.br/user/register/today-working",
+    url: "googlechrome://gestaoponto.sicredi.com.br/user/register/today-working",
     coop: true,
     cola: true,
     safari: true,
     chrome: true,
-    vpn: false
+    vpn: false,
+    logo: null
   },
   {
-    name: "ADMINISTRADOR DE CANAIS - ADMIN",
-    url: "http://admin-ibpj.sicredi.net/",
+    name: "ADMIN - ADMINISTRADOR DE CANAIS",
+    url: "googlechrome://admin-ibpj.sicredi.net/",
     coop: true,
     cola: false,
     safari: true,
     chrome: true,
-    vpn: false
+    vpn: false,
+    logo: null
   },
   {
     name: "BASE DE CONHECIMENTO",
-    url: "https://sicrediprod.service-now.com/0731",
+    url: "googlechromes://sicrediprod.service-now.com/0731",
     coop: false,
     cola: true,
     safari: true,
     chrome: true,
-    vpn: false
+    vpn: false,
+    logo: null
   },
   {
     name: "CRM",
-    url: "http://crmoperacional.sicredi.net/",
+    url: "googlechrome://crmoperacional.sicredi.net/",
     coop: true,
     cola: false,
     safari: true,
     chrome: true,
-    vpn: false
+    vpn: false,
+    logo: null
   },
   {
     name: "DOCUSIGN",
-    url: "https://account.docusign.com/#/password",
+    url: "googlechromes://account.docusign.com/#/password",
     coop: false,
     cola: true,
     safari: true,
     chrome: true,
-    vpn: false
+    vpn: false,
+    logo: "https://www.docusign.com.br/themes/custom/cubic/patternLibraryAssets/images/logos/logo-main.svg"
   },
   {
     name: "FLUID",
-    url: "http://cafelandia.websicredi.com.br",
+    url: "googlechrome://cafelandia.websicredi.com.br",
     coop: true,
     cola: true,
     safari: true,
     chrome: true,
-    vpn: false
+    vpn: false,
+    logo: "http://www.fluidnow.com.br//wp-content/uploads/2015/10/fluid.png"
   },
   {
-    name: "SISTEMA DE CONSÓRCIOS",
-    url: "https://consorcio.sicredi.com.br/",
+    name: "CONSÓRCIOS",
+    url: "googlechromes://consorcio.sicredi.com.br/",
     coop: true,
     cola: true,
     safari: true,
     chrome: true,
-    vpn: false
+    vpn: false,
+    logo: null
   },
   {
-    name: "SISTEMA DE SEGUROS - SIS",
-    url: "http://sis.sicredi.net/",
+    name: "SIS",
+    url: "googlechrome://sis.sicredi.net/",
     coop: true,
     cola: false,
     safari: true,
     chrome: true,
-    vpn: false
+    vpn: false,
+    logo: null
   },
   {
-    name: "SISTEMA DE GESTÃO DE IDENTIDADES - IDM",
-    url: "https://acessos.sicredi.net/",
+    name: "ACESSO INTELIGENTE",
+    url: "googlechromes://acessos.sicredi.net/",
     coop: true,
     cola: false,
     safari: false,
     chrome: true,
-    vpn: false
+    vpn: false,
+    logo: null
   },
   {
     name: "TAREFAS DA REDE COLABORATIVA",
-    url: "http://front-corp12c.sicredi.net/sicredi-tarefas-frontend/",
+    url: "googlechrome://front-corp12c.sicredi.net/sicredi-tarefas-frontend/",
     coop: true,
     cola: false,
     safari: true,
     chrome: true,
-    vpn: false
+    vpn: false,
+    logo: null
   },
   {
-    name: "SISITEMA DE CONSULTA - BUREAU",
-    url: "https://bureau.sicredi.net/",
+    name: "BUREAU",
+    url: "googlechromes://bureau.sicredi.net/",
     coop: true,
     cola: false,
     safari: false,
     chrome: true,
-    vpn: false
+    vpn: false,
+    logo: null
   },
   {
     name: "ANALYZER",
-    url: "http://pfc.sicredi.com.br",
+    url: "googlechrome://pfc.sicredi.com.br",
     coop: false,
     cola: false,
     safari: false,
     chrome: false,
-    vpn: true
+    vpn: true,
+    logo: null,
+    showModal: true
   },
   {
     name: "SISTEMAS HGV",
-    url: "http://sistemashgv.sicredi.net",
+    url: "googlechrome://sistemashgv.sicredi.net",
     coop: false,
     cola: false,
     safari: false,
     chrome: false,
-    vpn: true
+    vpn: true,
+    logo: null,
+    showModal: false
   },
   {
     name: "GESTÃO DE FILAS",
@@ -165,34 +184,38 @@ const links = [
     cola: false,
     safari: false,
     chrome: false,
-    vpn: false
+    vpn: false,
+    logo: null
   },
   {
     name: "GED - NE BANCO DE DOCUMENTOS",
-    url: "http://nebancodedocumentos.com.br/ged/login.php",
+    url: "googlechrome://nebancodedocumentos.com.br/ged/login.php",
     coop: true,
     cola: true,
     safari: true,
     chrome: true,
-    vpn: false
+    vpn: false,
+    logo: null
   },
   {
     name: "PLACECON",
-    url: "http://placecon.com.br",
+    url: "googlechromes://placecon.com.br",
     coop: true,
     cola: true,
     safari: true,
     chrome: true,
-    vpn: false
+    vpn: false,
+    logo: "https://placecon.com.br/auth/assets/images/logo_dark.png"
   },
   {
     name: "USD",
-    url: "http://usd.sicredi.net",
+    url: "googlechrome://usd.sicredi.net",
     coop: true,
     cola: false,
     safari: false,
     chrome: true,
-    vpn: false
+    vpn: false,
+    logo: null
   },
   {
     name: "VISUALIZE",
@@ -201,60 +224,69 @@ const links = [
     cola: false,
     safari: false,
     chrome: false,
-    vpn: false
+    vpn: false,
+    logo: null
   },
   {
     name: "AGENTE CREDENCIADO",
-    url: "https://bo-agente.sicredi.net/",
+    url: "googlechromes://bo-agente.sicredi.net/",
     coop: true,
     cola: false,
     safari: false,
     chrome: true,
-    vpn: false
+    vpn: false,
+    logo: null
   },
   {
     name: "WHATSAPP ENTERPRISE",
-    url: "https://plataforma.ubots.com.br/Sicredi",
+    url: "googlechromes://plataforma.ubots.com.br/Sicredi",
     coop: true,
     cola: true,
     safari: true,
     chrome: true,
-    vpn: false
+    vpn: false,
+    logo: "https://ubots.com.br/wp-content/uploads/2018/03/logo-branco-367px.png"
   },
   {
     name: "CETIP",
-    url: "https://cadu.cetip.com.br/login",
+    url: "googlechromes://cadu.cetip.com.br/login",
     coop: false,
     cola: true,
     safari: true,
     chrome: true,
-    vpn: false
+    vpn: false,
+    logo: "https://i.imgur.com/tJvUHmN.png"
   }
 ]
 
-
+/*
 const initFilter = {
   coop: false,
   cola: false,
   safari: false,
   chrome: false,
-  vpn: false
+  vpn: false,
+logo: null
 }
 
-
+*/
 
 
 function App() {
 
-  const [filter, setFilter] = useState(initFilter)
+  links = links.sort(function (a, b) {
+    if (a.name > b.name) {
+      return 1;
+    }
+    if (a.name < b.name) {
+      return -1;
+    }
+    return 0;
+  });
+
+  //const [filter, setFilter] = useState(initFilter)
   const [searchText, setSearchText] = useState('')
   const [filteredLinks, setFilteredLinks] = useState(links)
-
-
-
-  useEffect(() => {
-    setFilteredLinks(links)
-  }, [])
 
 
 
@@ -273,45 +305,46 @@ function App() {
       setFilteredLinks(links)
     }
   }
-
-  function handleInputChange(event) {
-    const target = event.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    const name = target.name;
-
-    setFilter({ ...filter, [name]: value });
-    const auxFilter = { ...filter, [name]: value }
-    if (isEquivalent(auxFilter, initFilter)) {
-      setFilteredLinks(links)
-      return
-    }
-    const result = links.filter(item => {
-      for (let key in auxFilter) {
-        if (item.key === false && (item[key] === undefined || item[key] !== auxFilter[key]))
-          return false
-        if (item.vpn !== auxFilter.vpn)
-          return false
+  /*
+    function handleInputChange(event) {
+      const target = event.target;
+      const value = target.type === 'checkbox' ? target.checked : target.value;
+      const name = target.name;
+  
+      setFilter({ ...filter, [name]: value });
+      const auxFilter = { ...filter, [name]: value }
+      if (isEquivalent(auxFilter, initFilter)) {
+        setFilteredLinks(links)``
+        return
       }
-      return true
-    })
-    setFilteredLinks(result)
-  }
-
-
-  function isEquivalent(a, b) {
-    var aProps = Object.getOwnPropertyNames(a);
-    var bProps = Object.getOwnPropertyNames(b);
-    if (aProps.length != bProps.length) {
-      return false;
+      const result = links.filter(item => {
+        for (let key in auxFilter) {
+          if (item.key === false && (item[key] === undefined || item[key] !== auxFilter[key]))
+            return false
+          if (item.vpn !== auxFilter.vpn)
+            return false
+        }
+        return true
+      })
+      setFilteredLinks(result)
     }
-    for (var i = 0; i < aProps.length; i++) {
-      var propName = aProps[i];
-      if (a[propName] !== b[propName]) {
+  */
+  /*
+    function isEquivalent(a, b) {
+      var aProps = Object.getOwnPropertyNames(a);
+      var bProps = Object.getOwnPropertyNames(b);
+      if (aProps.length != bProps.length) {
         return false;
       }
+      for (var i = 0; i < aProps.length; i++) {
+        var propName = aProps[i];
+        if (a[propName] !== b[propName]) {
+          return false;
+        }
+      }
+      return true;
     }
-    return true;
-  }
+    */
 
 
   return (
@@ -325,8 +358,19 @@ function App() {
       </header>
       <body >
         <div className="search-bar">
-          <input className="input-search" placeholder="Busque um sistema aqui" value={searchText} onChange={(event) => { filterLink(event) }} />
+          <input className="input-search" placeholder="Busque por um sistema aqui" value={searchText} onChange={(event) => { filterLink(event) }} />
           <FaSearch color="#aab0b5" />
+        </div>
+        <div className="index-container" >
+          <div className="index-item-container">
+            <FaChrome color="#414452" style={{ marginRight: 2 }} size={40} style={{ marginRight: 20 }} />
+            <strong>Disponivel no Google Chrome</strong>
+          </div >
+          <br />
+          <div className="index-item-container">
+            <FaSafari color="#414452" size={40} style={{ marginRight: 20 }} />
+            <strong>Disponivel no Safari</strong>
+          </div>
         </div>
         <div className="container">
           <div className="container-coop">
@@ -340,7 +384,13 @@ function App() {
                     <li>
                       <a href={item.url} target="_blank">
                         <div className="item-logo" >
-                          <img src="https://logodownload.org/wp-content/uploads/2017/11/sicredi-logo.png" />
+                          {
+                            item.logo !== null ?
+                              <img src={item.logo} />
+                              :
+                              <img src="https://logodownload.org/wp-content/uploads/2017/11/sicredi-logo.png" />
+
+                          }
                           <div className="container-icons">
                             {item.chrome && <FaChrome color="#414452" style={{ marginRight: 2 }} size={16} />}
                             {item.safari && <FaSafari color="#414452" size={16} />}
@@ -365,7 +415,13 @@ function App() {
                     <li>
                       <a href={item.url} target="_blank">
                         <div className="item-logo">
-                          <img src="https://logodownload.org/wp-content/uploads/2017/11/sicredi-logo.png" />
+                          {
+                            item.logo !== null ?
+                              <img src={item.logo} />
+                              :
+                              <img src="https://logodownload.org/wp-content/uploads/2017/11/sicredi-logo.png" />
+
+                          }
                           <div className="container-icons">
                             {item.chrome && <FaChrome color="#414452" style={{ marginRight: 2 }} size={16} />}
                             {item.safari && <FaSafari color="#414452" size={16} />}
@@ -386,19 +442,36 @@ function App() {
               {
                 filteredLinks.map(item =>
                   (item.vpn &&
-                    <li>
-                      <a href={item.url} target="_blank">
+                    <div style={{ display: "flex", alignItems: "center" }}>
+                      <li key={item.url} >
                         <div className="item-logo">
-                          <img src="https://logodownload.org/wp-content/uploads/2017/11/sicredi-logo.png" />
+                          {
+                            item.logo !== null ?
+                              <img src={item.logo} />
+                              :
+                              <img src="https://logodownload.org/wp-content/uploads/2017/11/sicredi-logo.png" />
+
+                          }
                           <div className="container-icons">
                             {item.chrome && <FaChrome color="#414452" style={{ marginRight: 2 }} size={16} />}
                             {item.safari && <FaSafari color="#414452" size={16} />}
                             {item.vpn && <span style={{ color: "#414452", fontWeight: "bold" }}>VPN</span>}
                           </div>
+
+                          <div >
+
+                          </div>
                         </div>
                         <div className="item-text">{item.name}</div>
-                      </a>
-                    </li>
+                      </li>
+
+                      <div className="modal-container">
+                        <strong style={{ color: "#fff" }}>Este link é acessível somente pela VPN</strong>
+                        <br />
+                        <br />
+                        <span>{item.url}</span>
+                      </div>
+                    </div>
                   )
                 )
               }
